57a018bd88127fdaf1172fbec0d9ca7865482cf3
rui1199 <61785629+rui1199@users.noreply.github.com>
2020-04-20 22:58:27 +0100

    ja adivinha o numero

--- app/src/main/java/pt/ipg/adivinha/MainActivity.java
+++ app/src/main/java/pt/ipg/adivinha/MainActivity.java
@@ -2,9 +2,11 @@ package pt.ipg.adivinha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int soma;
     public void onClickBotaosubmeter (View view) {
         EditText textInputNumero = (EditText) findViewById(R.id.textInputNumero);
 
@@ -29,19 +32,28 @@ public class MainActivity extends AppCompatActivity {
         try {
             numero = Integer.parseInt(numero1);
         }catch (NumberFormatException e){
-            textInputNumero.setError("diga um numero de 0 a 10");
+            textInputNumero.setError(getString(R.string.numero_ob));
             textInputNumero.requestFocus();
             return;
         }
         if(numero <0||numero>10){
-            textInputNumero.setError("tem de ser entre 0 e 10");
+            textInputNumero.setError(getString(R.string.numero_lim));
             textInputNumero.requestFocus();
             return;
         }
-        if(numero==numeroadivinha){
-            textViewresultado.setText("Correto");
-        }else{
-            textViewresultado.setText("Errado");
+        TextView textViewtentativas = findViewById(R.id.textViewtentativas);
+        TextView textViewresultado = findViewById(R.id.textViewresultado);
+
+        if(numeroadivinha == numero ){
+            textViewresultado.setText(R.string.resultado_certo);
+            textViewresultado.setTextColor(Color.BLUE);
+            textViewresultado.requestFocus();
+        } else{
+            soma++;
+            textViewresultado.setText(R.string.resultado_errado);
+            textViewresultado.setTextColor(Color.RED);
+            textViewtentativas.setText(String.valueOf(soma));
         }
+
     }
 }
